a4ecf19b1b36b8c78675a149457b0882646b13da
meganasampson <megan.anne.sampson@gmail.com>
2024-06-13 15:19:41 +0100

    mini cleanup

--- directorymaker.py
+++ directorymaker.py
@@ -8,47 +8,53 @@ finalstructure = []
 
 @click.command()
 @click.option('--filename', default='structureexample.txt', help='file containing desired option, default = structureexample.md')
-@click.option('--opsys', prompt='os?', help='os file structure, 1 for linux, 2 for windows')
 
-def get_dictionary_word_list(filename, opsys):
+def get_dictionary_word_list(filename):
     """
     open file with the file structure and put in a list
     """
     f = open(filename)
     for word in f.read().split():
         filelist.append(word)
-    print(filelist)
-    splitter(filelist)
+    folder_maker(filelist)
 
-def splitter(filelist):
-    level = int(1)
+
+def folder_maker(filelist):
+    """
+    loops through file structure list and makes folders based on ├── symbol
+    """
+    level = 1
+
     for i in range(len(filelist)):
+
         if "├──" not in filelist[i]:
-            level2 = int(filelist[i-1].count("├──"))
+            #checks where the next folder should be based on current level
+            level2 = filelist[i-1].count("├──")
+
             if level2 == level:
+                #make folder in same directory
                 os.makedirs(filelist[i])
+
             elif level2 > level:
+                #moves DOWN next directory and makes folder in there
                 cwd = Path(os.getcwd())
                 foldername = Path(filelist[i-2])
                 newpath = cwd/foldername
                 os.chdir(newpath)
                 os.makedirs(filelist[i])
+                #make new level the current level
                 level = level + 1
-            elif level2 < level:
-                #for j in range(level - level2):
-                cwd = Path(os.getcwd())
-                foldername = Path("..")
-                newpath = cwd/foldername
-                #newpath = os.path.abspath(os.path.join(cwd, os.pardir))
-                os.chdir(newpath)
-                print(os.getcwd())
-                os.makedirs(filelist[i])
-                print(filelist[i])
-                level = level2
-            print(level)
-            print(filelist[i])
-                
 
+            elif level2 < level:
+                for j in range(level - level2):
+                    #moves UP to the correct directory and makes folder there
+                    cwd = Path(os.getcwd())
+                    foldername = Path("..")
+                    newpath = cwd/foldername
+                    os.chdir(newpath)
+                os.makedirs(filelist[i])
+                #make new level the current level
+                level = level2
 
 
 if __name__ == '__main__':
