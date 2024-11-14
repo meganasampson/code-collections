60bf5be085279f4b5be01a7d3cdccd67cb52114a
meganasampson <megan.anne.sampson@gmail.com>
2024-11-14 10:27:40 +0000

    updated structure example

--- directorymaker.py
+++ directorymaker.py
@@ -18,10 +18,10 @@ def get_dictionary_word_list(filename, location):
     open file with the file structure and put in a list
     """
     f = open(filename)
-    for word in f.read().replace(" ", "").replace("│","├──").split():
+    for word in f.read().replace("   ", "").replace("│","├──").split():
         filelist.append(word)
     print(filelist)
-    folder_maker(filelist, location)
+    #folder_maker(filelist, location)
 
 
 def folder_maker(filelist, location):
